2efba3bc33b0d9c8afde386991bf900f3c76701b
isedsonadriano <matracaweb@gmail.com>
2021-01-16 08:22:26 -0300

    refactor:  melhoria geral

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,17 +1,15 @@
 import { HttpRequest, HttpResponse } from "../protocols/http";
-import {MissingParamError} from '../erros/erros-param-error'
-import {badRequest} from '../helpers/http-helper'
+import { MissingParamError } from "../erros/erros-param-error";
+import { badRequest } from "../helpers/http-helper";
 
 export class SignUpController {
   handle(httpRequest: HttpRequest): HttpResponse {
+    const requireFields = ["name", "email", "password"];
 
-    const requireFields = ['name', 'email']
-
-    for(const field of requireFields){
-        if (!httpRequest.body[field]) {
-            return badRequest(new MissingParamError(field))
-        }
+    for (const field of requireFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field));
+      }
     }
-
   }
 }
