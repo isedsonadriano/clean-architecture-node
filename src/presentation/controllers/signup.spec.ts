1a511251dd671ac60e1dbc52e3a9c641754a17ca
isedsonadriano <matracaweb@gmail.com>
2021-01-22 07:25:16 -0300

    refactor: alteraçao nos mocks

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -17,15 +17,6 @@ const makeEmailValidator = (): EmailValidator => {
   return new EmailValidatorStub()
 }
 
-const makeEmailValidatorWithError = (): EmailValidator => {
-  class EmailValidatorStub implements EmailValidator{
-    isValid(email:string): boolean{
-      throw new Error()
-    }
-  }
-  return new EmailValidatorStub()
-}
-
 const makeSut = (): any =>{
  
   const emailValidatorStub = makeEmailValidator()
@@ -127,8 +118,10 @@ describe("SignUp Controller", () => {
   });
 
   test("Should return 500 if email validator throws", () => {
-    const emailValidatorStub = makeEmailValidatorWithError()
-    const sut = new SignUpController(emailValidatorStub)
+    const {sut, emailValidatorStub} = makeSut();
+    jest.spyOn(emailValidatorStub, 'isValid').mockImplementationOnce(() =>{
+      throw new Error()
+  })
       
     const httpRequest = {
       body: {
@@ -142,5 +135,7 @@ describe("SignUp Controller", () => {
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body).toEqual(new ServerError())
   });
+
+  
 
 });
