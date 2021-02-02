6dfa64315b7e3068767602047adeeb46b4ba78ff
isedsonadriano <matracaweb@gmail.com>
2021-02-02 19:22:55 -0300

    refactor: ajustes no test

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,14 +1,27 @@
+import { Encryper } from "../../../presentation/protocols/encrypter"
 import { DbAddAccount } from "./db-add-account"
 
+interface SutTypes{
+    sut: DbAddAccount
+    encrypterStub: Encryper
+}
+
+const makeSut = (): SutTypes =>{
+    class EncryperStub{
+        async encrypt(value:string): Promise<string>{
+            return new Promise(resolve => resolve('hashed_password'))
+        }
+    }
+    const encrypterStub  = new EncryperStub()
+    const sut = new DbAddAccount(encrypterStub)
+    return {
+        sut,
+        encrypterStub
+    }
+}
 describe('DbAaddAccount UseCase', () => {
     test('Should call Encrypter with correct password', async () =>{
-        class EncryperStub{
-            async encrypt(value:string): Promise<string>{
-                return new Promise(resolve => resolve('hashed_password'))
-            }
-        }
-        const encrypterStub  = new EncryperStub()
-        const sut = new DbAddAccount(encrypterStub)
+        const {sut, encrypterStub} = makeSut()
         const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
         const accountData = {
             name: 'valid_name',
